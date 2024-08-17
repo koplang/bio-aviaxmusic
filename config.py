830a31308c7b2a44f3ac23c509ee2f0a9c1c76eb
bioslen <157968855+bioslen@users.noreply.github.com>
2024-08-17 14:19:12 +0700

    config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/log_brisik)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/log_brisik")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/log_brisik")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
